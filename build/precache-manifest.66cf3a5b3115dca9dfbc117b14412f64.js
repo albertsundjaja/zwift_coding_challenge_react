8c6ae2836cd878c0521c106a4859af348f45d5ea
Albert <grandia3@hotmail.com>
2019-12-19 12:03:13 +1100

    added built version

---
+++ build/precache-manifest.66cf3a5b3115dca9dfbc117b14412f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41f92564f03a381d593a169aca31da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a819d4fe142e58c50bdd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "14d035a91ba85a0e3c9a",
+    "url": "/static/js/2.8bd98a04.chunk.js"
+  },
+  {
+    "revision": "2570fa9c0875b6220cda3119a6bbef60",
+    "url": "/static/js/2.8bd98a04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a819d4fe142e58c50bdd",
+    "url": "/static/js/main.87a930f6.chunk.js"
+  },
+  {
+    "revision": "f53602c7d20bebd6e16c",
+    "url": "/static/js/runtime-main.e15edac2.js"
+  }
+]);
